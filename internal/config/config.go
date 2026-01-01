2a58c3b2c47df4f109d9b9302e3cfdb1230891e9
Konstantin <kos.oraz@gmail.com>
2026-01-01 19:54:51 +0300

    add route, handler, service

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,36 @@
 package config
 
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type Config struct {
-	AppPort  string `mapstructure:"APP_PORT"`
-	DBUrl    string `mapstructure:"DB_URL"`
-	LogLevel string `mapstructure:"LOG_LEVEL"`
+	Env         string `yaml:"env" env:"ENV" env-default:"local" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Addr        string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := "config/local.yaml"
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("CONFIG_PATH does not exist:", configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatal("cannot read config:", err)
+	}
+
+	return &cfg
 }
